ac830b32592057d7c1b3d5fae6f960650ca086a4
erpindi <61915300+erpindi@users.noreply.github.com>
2020-10-15 10:47:44 +0700

    Create CurrencyControlel.cs

---
+++ CurrencyApps/CurrencyControlel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Currency_Apps
+{
+    class CurrencyControlel
+    {
+        public string usdToIdr(string nominal)
+        {
+            var nominalDouble = Convert.ToDouble(nominal);
+            var result = nominalDouble * 15000;
+            return Convert.ToString(result);
+        }
+
+        public Boolean isAllowed(string nominal)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            return !regex.IsMatch(nominal);
+        }
+    }
+}
