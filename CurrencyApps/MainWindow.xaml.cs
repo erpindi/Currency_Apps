c290e1c653873840638d79a54d7c575ca07b2303
erpindi <61915300+erpindi@users.noreply.github.com>
2020-10-15 10:50:21 +0700

    Create MainWindow.xaml.cs

---
+++ CurrencyApps/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+namespace Currency_Apps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CurrencyControlel currency;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            currency = new CurrencyControlel();
+        }
+        private void Button_Hitung_Click(object sender, RoutedEventArgs e)
+        {
+            var nominalInput = InputTextBox.Text;
+            var result = "invalid";
+            if (currency.isAllowed(nominalInput))
+            {
+                result = currency.usdToIdr(nominalInput);
+            }
+            resultLabel.Content = result;
+
+        }
+    }
+}
